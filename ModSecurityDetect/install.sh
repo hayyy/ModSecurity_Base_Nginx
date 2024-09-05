69a193e02e6db2037d2b7df3a998f9002f57c61f
hay <945772622@qq.com>
2024-09-05 14:26:37 +0800

    安装脚本新增安装waf规则

--- ModSecurityDetect/install.sh
+++ ModSecurityDetect/install.sh
@@ -23,6 +23,18 @@ make
 make install
 cd ${CUR_DIR}
 
+# cmake
+wget https://cmake.org/files/v3.29/cmake-3.29.7.tar.gz
+tar xzvf cmake-3.29.7.tar.gz
+apt-get install doxygen
+cd cmake-3.29.7
+./bootstrap
+make
+make install
+rm -f /usr/bin/cmake
+ln -s /usr/local/bin/cmake /usr/bin/cmake
+cd ${CUR_DIR}
+
 # 安装iniparser
 cd iniparser
 mkdir build
@@ -47,4 +59,29 @@ make
 make install
 cd ${CUR_DIR}
 
+# 编译firewall_detect
+cd ..
+export LD_LIBRARY_PATH=/usr/local/modsecurity/lib:/usr/local/lib:$LD_LIBRARY_PATH
 make
+mkdir -p /etc/modSecurityDetect/
+cp -rf config /etc/modSecurityDetect/
+mkdir -p /var/log/modSecurityDetect/
+cd ${CUR_DIR}
+
+#安装waf规则
+wget http://www.modsecurity.cn/download/corerule/owasp-modsecurity-crs-3.3-dev.zip
+unzip owasp-modsecurity-crs-3.3-dev.zip
+cd owasp-modsecurity-crs-3.3-dev
+cp crs-setup.conf.example /etc/modSecurityDetect/config/crs-setup.conf
+
+cp -rf rules /etc/modSecurityDetect/
+cd /etc/modSecurityDetect/rules
+cp REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf.example REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf
+cp RESPONSE-999-EXCLUSION-RULES-AFTER-CRS.conf.example RESPONSE-999-EXCLUSION-RULES-AFTER-CRS.conf
+cd -
+
+cp ${CUR_DIR}/modsecurity-v3.0.4/modsecurity.conf-recommended /etc/modSecurityDetect/config/modsecurity.conf
+cp ${CUR_DIR}/modsecurity-v3.0.4/unicode.mapping /etc/modSecurityDetect/config/
+
+echo "Include /etc/modSecurityDetect/config/crs-setup.conf" >> /etc/modSecurityDetect/config/modsecurity.conf
+echo "Include /etc/modSecurityDetect/rules/*.conf" >> /etc/modSecurityDetect/config/modsecurity.conf
